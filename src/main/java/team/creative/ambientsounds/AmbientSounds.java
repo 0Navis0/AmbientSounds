d22176dba6a4b4921294d90f58ee00e3f9f32b28
CreativeMD <CreativeMD@users.noreply.github.com>
2024-09-16 04:57:00 -0400

    Merge pull request #250 from Steveplays28/fix-async-engine-reload
    Fix async engine reloading

--- src/main/java/team/creative/ambientsounds/AmbientSounds.java
+++ src/main/java/team/creative/ambientsounds/AmbientSounds.java
@@ -1,16 +1,20 @@
 package team.creative.ambientsounds;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraftforge.fml.common.Mod;
 import team.creative.ambientsounds.engine.AmbientEngine;
@@ -20,34 +24,36 @@ import team.creative.creativecore.ICreativeLoader;
 import team.creative.creativecore.client.ClientLoader;
 import team.creative.creativecore.client.CreativeCoreClient;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-
 @Mod(value = AmbientSounds.MODID)
 public class AmbientSounds implements ClientLoader {
-
+    
     public static final Logger LOGGER = LogManager.getLogger(AmbientSounds.MODID);
     public static final String MODID = "ambientsounds";
     public static final AmbientSoundsConfig CONFIG = new AmbientSoundsConfig();
+    
     public static AmbientTickHandler TICK_HANDLER;
-
+    
     public AmbientSounds() {
         ICreativeLoader loader = CreativeCore.loader();
         loader.registerClient(this);
     }
-
+    
     public static void scheduleReload() {
         TICK_HANDLER.scheduleReload();
     }
-
-    public static void reload() {
+    
+    public static void reloadAsync() {
+        CompletableFuture.runAsync(AmbientSounds::reload, Util.backgroundExecutor());
+    }
+    
+    private static synchronized void reload() {
         if (TICK_HANDLER.engine != null)
             TICK_HANDLER.engine.stopEngine();
         if (TICK_HANDLER.environment != null)
             TICK_HANDLER.environment.reload();
         TICK_HANDLER.setEngine(AmbientEngine.loadAmbientEngine(TICK_HANDLER.soundEngine));
     }
-
+    
     @Override
     public void onInitializeClient() {
         ICreativeLoader loader = CreativeCore.loader();
@@ -57,28 +63,28 @@ public class AmbientSounds implements ClientLoader {
         loader.registerClientTick(TICK_HANDLER::onTick);
         loader.registerClientRenderGui(TICK_HANDLER::onRender);
         loader.registerLoadLevel(TICK_HANDLER::loadLevel);
-
+        
         loader.registerClientStarted(() -> {
             Minecraft minecraft = Minecraft.getInstance();
             ReloadableResourceManager reloadableResourceManager = (ReloadableResourceManager) minecraft.getResourceManager();
-
-            reloadableResourceManager.registerReloadListener(new PreparableReloadListener() {
-
+            
+            reloadableResourceManager.registerReloadListener(new SimplePreparableReloadListener<Void>() {
                 @Override
-                public CompletableFuture<Void> reload(PreparationBarrier preparationBarrier, ResourceManager resourceManager, ProfilerFiller prepareProfiler, ProfilerFiller applyProfiler, Executor prepareExecutor, Executor applyExecutor) {
-	                return CompletableFuture.supplyAsync(() -> {
-		                AmbientSounds.reload();
-
-                        preparationBarrier.wait(null);
-	                    return null;
-	                }, applyExecutor);
+                protected Void prepare(@NotNull ResourceManager resourceManager, @NotNull ProfilerFiller profilerFiller) {
+                    AmbientSounds.reloadAsync();
+                    return null;
+                }
+                
+                @Override
+                protected void apply(Void object, @NotNull ResourceManager resourceManager, @NotNull ProfilerFiller profilerFiller) {
+                    // NO-OP
                 }
             });
         });
-
+        
         CreativeCoreClient.registerClientConfig(MODID);
     }
-
+    
     @Override
     public <T> void registerClientCommands(CommandDispatcher<T> dispatcher) {
         dispatcher.register(LiteralArgumentBuilder.<T>literal("ambient-debug").executes(x -> {
@@ -86,9 +92,9 @@ public class AmbientSounds implements ClientLoader {
             return Command.SINGLE_SUCCESS;
         }));
         dispatcher.register(LiteralArgumentBuilder.<T>literal("ambient-reload").executes(x -> {
-            AmbientSounds.reload();
+            AmbientSounds.reloadAsync();
             return Command.SINGLE_SUCCESS;
         }));
     }
-
+    
 }
